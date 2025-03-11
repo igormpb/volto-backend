be6ed8128f5b40b4141965194256a9a80b357fe6
igor <igor.mpb@hotmail.com>
2025-03-10 21:58:25 -0300

    fix

--- src/main/java/com/igormpb/voltoja/infra/adapter/card/StripeCard.java
+++ src/main/java/com/igormpb/voltoja/infra/adapter/card/StripeCard.java
@@ -6,9 +6,14 @@ import com.igormpb.voltoja.domain.response.CreateCheckoutPageAdapterResponse;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
+import com.stripe.net.RequestOptions;
 import com.stripe.param.checkout.SessionCreateParams;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 
 @Component
@@ -19,12 +24,12 @@ public class StripeCard implements ICardAdapter {
     }
 
     @Override
-    public CreateCheckoutPageAdapterResponse CreateCheckoutPage(Long price, String name) throws HandleErros{
+    public CreateCheckoutPageAdapterResponse CreateCheckoutPage(Long price, String name) throws HandleErros {
         try {
-         SessionCreateParams params = SessionCreateParams.builder()
+            SessionCreateParams params = SessionCreateParams.builder()
                     .setMode(SessionCreateParams.Mode.PAYMENT)
-                    .setSuccessUrl("https://seu-dominio.com/sucesso")
-                    .setCancelUrl("https://seu-dominio.com/cancelado")
+                    .setSuccessUrl("https://flashja.digital/sucessor")
+                    .setCancelUrl("https://flashja.digital/error")
                     .addLineItem(
                             SessionCreateParams.LineItem.builder().setQuantity(1L)
                                     .setPriceData(
@@ -34,18 +39,24 @@ public class StripeCard implements ICardAdapter {
                                                             SessionCreateParams.LineItem.PriceData.ProductData.builder()
                                                                     .setName(name)
                                                                     .build()
+
                                                     )
                                                     .setUnitAmount(price)
+
                                                     .build()
                                     )
                                     .build()
                     )
                     .build();
-            Session session = Session.create(params);
 
-            var response = new CreateCheckoutPageAdapterResponse(session.getPaymentIntent(), session.getUrl(), session.getAmountTotal(), session.getPaymentStatus());
+            String idempotencyKey = UUID.randomUUID().toString();
+
+            var requestOptions = RequestOptions.builder().setIdempotencyKey(idempotencyKey).build();
+            Session session = Session.create(params, requestOptions);
+
+            var response = new CreateCheckoutPageAdapterResponse(idempotencyKey, session.getUrl(), session.getAmountTotal(), session.getPaymentStatus());
             return response;
-        }catch (StripeException e) {
+        } catch (StripeException e) {
             System.out.println(e.getMessage());
             throw new HandleErros("Não foi possível criar um pagamento", HttpStatus.NOT_FOUND);
         }
